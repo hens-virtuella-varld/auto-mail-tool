5b88ef35255e98209a98d21e9e703f1d7efbc7ac
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2019-04-23 13:53:29 +0800

    Publish the initial version of project.
    Add auto-mail-program, mail-content, mail-list files.
    Update README.

---
+++ auto-mail-program.py
@@ -0,0 +1,28 @@
+import csv
+import smtplib
+from email.mime.text import MIMEText
+
+gmail_user = 'your-gmail-id@gmail.com'  # Type your gmail ID
+gmail_password = 'your-password'  # Type your gmail password
+
+with open('mail-content.txt') as txtfile:
+    content = txtfile.read()
+
+
+def send_mail_one_receiver(receiver):
+    msg = MIMEText(content)
+    msg['Subject'] = 'your-mail-subject'  # Type your mail subject
+    msg['From'] = gmail_user
+    msg['To'] = receiver
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    server.send_message(msg)
+    server.quit()
+
+
+with open('mail-list.csv') as csvfile:
+    rows = csv.reader(csvfile)
+    for receiver in rows:
+        send_mail_one_receiver(receiver[0])
+    print('Email sent!')
